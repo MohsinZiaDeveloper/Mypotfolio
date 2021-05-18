546599e642022a83657e8ee182bdfec34ba66d58
mohsinzia-01 <mohsinziaariduni1@gmail.com>
2021-05-18 23:05:50 +0500

    inital commit added Navbar component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+//Components
+import Mynavbar from "./Components/my-navbar/mynavbar.component";
+
+const App =() => {
+  return (
+    <div >
+     <Mynavbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/my-navbar/mynavbar.component.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Logo from '../../assets/icons/logow.webp';
+import './mynavbar.style.css';
+
+const Mynavbar = () => {
+    return (
+        <>
+        <Navbar fixed="top" collapseOnSelect expand="md"  variant="dark" className="animate-navbar nav-theme justify-content-between">
+        <Navbar.Brand href="#home">
+        <img  className="logo" src={Logo} alt="My Potfolio Logo "/>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+         <Nav className="ml-auto">
+            <Nav.Link className="linkgap" href="#home">Home</Nav.Link>
+            <Nav.Link className="linkgap" href="#about">About</Nav.Link>
+            <Nav.Link className="linkgap" href="#skills">Skills</Nav.Link>
+            <Nav.Link className="linkgap" href="#experience">Experience</Nav.Link>
+            <Nav.Link className="linkgap" href="#projects">Projects</Nav.Link>
+            <Nav.Link className="linkgap" href="#contact">Contact</Nav.Link>
+        </Nav>
+        
+        </Navbar.Collapse>
+        </Navbar>
+            
+        </>
+    )
+}
+
+export default Mynavbar
