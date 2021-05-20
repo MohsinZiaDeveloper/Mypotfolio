d0a65606e3ceed5a294b75ccacdbdffc0e777ea6
mohsinzia-01 <mohsinziaariduni1@gmail.com>
2021-05-21 01:14:47 +0500

    added final Upload

--- src/App.js
+++ src/App.js
@@ -14,15 +14,22 @@ import Image from './assets/img/background/background.webp';
 import Skills from "./Pages/skills/skills.component";
 import Experience from "./Pages/experience/experience.component";
 import TimeLine from "./Components/project-timeline/project-timeline.component";
+import ContactForm from "./Pages/contact-form/contact-form.component";
+import FooterPanel from "./Components/footer/footer.component";
+import Particles from "react-particles-js";
+import { particlesOptions } from "./particlesOptions";
 
 
 const App =() => {
   return (
 
     <div className="App" style={{ position: "relative" }} >
-     <Mynavbar />
+
+      
      <Mycarousal />
      <TitleMassage />
+     <Mynavbar />
+     <Particles className="particles particles-box" params={particlesOptions}/>
 
     <div> 
       <Parallax blur={{ min: -30, max: 30 }} bgImage={Image} bgImageAlt="" strength={-200} > 
@@ -55,7 +62,16 @@ const App =() => {
           <TimeLine />
         </Slide>
       </Container>
+
+      <Container className="container-box rounded">
+        <Fade duration={500}>
+          <hr />
+          <ContactForm />
+        </Fade>
+      </Container>
    
+      <hr />
+      <FooterPanel />
 
 
   
